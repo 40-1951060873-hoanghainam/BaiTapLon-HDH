c968be8f1193d6b9776c4997d65fcdbe6d94f993
40-1951060873-hoanghainam <hoanghainam14812@gmail.com>
2022-04-01 22:56:10 +0700

    tao list cac chuong trinh

--- BaiTapLon-HDH/Form1.cs
+++ BaiTapLon-HDH/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,27 @@ namespace BaiTapLon_HDH
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //Start
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = startTextBox.Text;
+            Process process = new Process();
+            process.StartInfo.FileName = text;
+            process.Start();
+        }
+
+        //List
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process[] processlist = Process.GetProcesses();
+            foreach(Process process in processlist)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.Tag = process;
+                listView1.Items.Add(item);
+            }
         }
     }
 }

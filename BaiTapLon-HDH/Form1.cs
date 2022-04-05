3083e552d15a36adfe72ad284e4681b121143e54
nguyentienchientlu2002@gmail.com <nguyentientruong0201@gmail.com>
2022-04-05 10:13:32 +0700

    install

--- BaiTapLon-HDH/Form1.cs
+++ BaiTapLon-HDH/Form1.cs
@@ -1,42 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BaiTapLon_HDH
+public class Form1 : Form
 {
-    public partial class Form1 : Form
+    private MenuStrip menuStrip1 = new MenuStrip();
+    private ToolStripMenuItem menu1 = new ToolStripMenuItem();
+    private ToolStripMenuItem fileMenuItem = new ToolStripMenuItem();
+    public void FormLayout()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        //Start
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string text = startTextBox.Text;
-            Process process = new Process();
-            process.StartInfo.FileName = text;
-            process.Start();
-        }
-
-        //List
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            Process[] processlist = Process.GetProcesses();
-            foreach(Process process in processlist)
-            {
-                ListViewItem item = new ListViewItem(process.ProcessName);
-                item.Tag = process;
-                listView1.Items.Add(item);
-            }
-        }
+        Form();
+        Menus();
     }
+    
+    void Form()
+    {
+        this.Name = "Form1";
+        this.Text = "Form1";
+        this.BackColor = System.Drawing.Color.Black;
+        this.Size = new System.Drawing.Size(500, 500);
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.StartPosition = FormStartPosition.CenterScreen;
+        this.KeyPreview = true;
+    }  
+    void Menus()
+    {
+        // menuStrip1
+        this.menuStrip1.Items.AddRange(new ToolStripItem[] 
+        {
+            menu1
+        });
+        this.menuStrip1.BackColor = System.Drawing.Color.Black;
+        this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+        this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+        this.menuStrip1.Name = "menuStrip1";
+        
+        // Menu1
+        this.menu1.Image = System.Drawing.Image.FromFile("images/menuHamburger.png");
+        this.menu1.Size = new System.Drawing.Size(180, 20);
+        this.menu1.Name = "menu1";   
+        this.menu1.AutoSize = false;        
+        this.menu1.DropDownItems.AddRange(new ToolStripItem[] 
+        {
+            this.fileMenuItem
+        });
+
+        this.fileMenuItem.Name = "fileMenuItem";
+        this.fileMenuItem.AutoSize = false;
+        this.fileMenuItem.Size = new System.Drawing.Size(180, 20);
+        this.fileMenuItem.Text = "Exit";
+        this.fileMenuItem.Click += new System.EventHandler(this.Mouse_Click);
+        this.Controls.Add(this.menuStrip1);
+    }
+
+    void Mouse_Click(object sender, EventArgs e)
+    {
+        Application.Exit();
+    }
+    
+           
 }

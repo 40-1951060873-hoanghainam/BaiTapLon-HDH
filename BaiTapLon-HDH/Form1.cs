460b3301842ed02af66fa7f772b7f84c60731ed2
nguyentienchientlu2002@gmail.com <nguyentientruong0201@gmail.com>
2022-04-05 10:20:04 +0700

    commit exit

--- BaiTapLon-HDH/Form1.cs
+++ BaiTapLon-HDH/Form1.cs
@@ -1,61 +1,48 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Diagnostics;
 
-public class Form1 : Form
+namespace example
 {
-    private MenuStrip menuStrip1 = new MenuStrip();
-    private ToolStripMenuItem menu1 = new ToolStripMenuItem();
-    private ToolStripMenuItem fileMenuItem = new ToolStripMenuItem();
-    public void FormLayout()
+    public partial class Form1 : Form
     {
-        Form();
-        Menus();
-    }
-    
-    void Form()
-    {
-        this.Name = "Form1";
-        this.Text = "Form1";
-        this.BackColor = System.Drawing.Color.Black;
-        this.Size = new System.Drawing.Size(500, 500);
-        this.FormBorderStyle = FormBorderStyle.None;
-        this.StartPosition = FormStartPosition.CenterScreen;
-        this.KeyPreview = true;
-    }  
-    void Menus()
-    {
-        // menuStrip1
-        this.menuStrip1.Items.AddRange(new ToolStripItem[] 
+        public Form1()
         {
-            menu1
-        });
-        this.menuStrip1.BackColor = System.Drawing.Color.Black;
-        this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-        this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-        this.menuStrip1.Name = "menuStrip1";
-        
-        // Menu1
-        this.menu1.Image = System.Drawing.Image.FromFile("images/menuHamburger.png");
-        this.menu1.Size = new System.Drawing.Size(180, 20);
-        this.menu1.Name = "menu1";   
-        this.menu1.AutoSize = false;        
-        this.menu1.DropDownItems.AddRange(new ToolStripItem[] 
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
         {
-            this.fileMenuItem
-        });
+            string text = startTextbox.Text;
+            Process process = new Process();
+            process.StartInfo.FileName = text;
+            process.Start();
+            loadProcessList();
+        }
 
-        this.fileMenuItem.Name = "fileMenuItem";
-        this.fileMenuItem.AutoSize = false;
-        this.fileMenuItem.Size = new System.Drawing.Size(180, 20);
-        this.fileMenuItem.Text = "Exit";
-        this.fileMenuItem.Click += new System.EventHandler(this.Mouse_Click);
-        this.Controls.Add(this.menuStrip1);
-    }
-
-    void Mouse_Click(object sender, EventArgs e)
-    {
-        Application.Exit();
-    }
-    
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadProcessList();
            
+
+        }
+
+        private void loadProcessList()
+        {
+            listView1.Items.Clear();
+            Process[] processList = Process.GetProcesses();
+            foreach (Process process in processList)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.Tag = process;
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+           ListViewItem item = listView1.SelectedItems[0];
+           Process process = (Process)item.Tag;
+           process.Kill();
+           loadProcessList();
+        }
+    }
 }
